aa235c671c4bbb9d6c9a49fd869415e8b9b1b5b1
入江夏輝 <natsuki.irie@irienatsukinoMacBook-Pro.local>
2017-12-12 17:31:16 +0900

    ルーティング、コントローラー、ビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # root 'users#edit'
   # root 'messages#index'
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :show, :edit, :update] do
     resources :messages, only: [:index, :new, :create]
   end
