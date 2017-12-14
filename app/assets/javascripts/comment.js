0f89131a5e246ab009f647d68da66a48bdb0218f
入江夏輝 <natsuki.irie@irienatsukinoMacBook-Pro.local>
2017-12-14 15:37:35 +0900

    メッセージ送信機能の調整

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,21 +1,23 @@
 $(document).on('turbolinks:load', function(){
   $(document).ready(function() {
     function buildHTML(message){
-      var html =       `<div class="chat-upper">
-                          <div class="chat-upper__name">
-                            ${message.name}
+      var html =       `<div class="chat-messages" data-id=${message.id}>
+                          <div class="chat-upper">
+                            <div class="chat-upper__name">
+                              ${message.name}
+                            </div>
+                            <div class="chat-upper__date">
+                              ${message.created_at}
+                            </div>
                           </div>
-                          <div class="chat-upper__date">
-                            ${message.created_at}
+                          <div class="chat-lower-message">
+                            <p class="chat-lower-content">
+                              ${message.text}
+                            </p>
+                            <p class="chat-lower-content-image">
+                              ${message.image}
+                            </p>
                           </div>
-                        </div>
-                        <div class="chat-lower-message">
-                          <p class="chat-lower-content">
-                            ${message.text}
-                          </p>
-                          <p class="chat-lower-content-image">
-                            ${message.image}
-                          </p>
                         </div>`
       return html;
     }
@@ -36,7 +38,7 @@ $(document).on('turbolinks:load', function(){
       })
       .done(function(data){
         var html = buildHTML(data);
-        $('.chat-messeges').append(html);
+        $('.chat-center-view').append(html);
         $('.message-form__field').val('');
         $(function() {
           $('.chat-center-view').animate({scrollTop: $('.chat-center-view')[0].scrollHeight}, 'fast');
